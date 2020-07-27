4d88aa0fa8681ce435f5765c3e02db57d39b575f
Mac9330 <macyoung94@gmail.com>
2020-07-27 17:47:17 -0400

    Heroku prep

--- frontend/sparebnb.jsx
+++ frontend/sparebnb.jsx
@@ -5,5 +5,5 @@ import ReactDOM from "react-dom";
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
     // const store = configureStore()
-    ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+    ReactDOM.render(<h1>Welcome to SpareBnB</h1>, root);
 });
