7763f94a8c1786902b44b30af6c7beb7344f04c6
Mac9330 <macyoung94@gmail.com>
2020-09-11 20:03:41 -0400

    removed comma

--- app/models/amenity.rb
+++ app/models/amenity.rb
@@ -3,7 +3,7 @@ class Amenity < ApplicationRecord
     validates :name, presence: true, uniqueness: true
     validates :icon, presence: true;
 
-    has_many :spot_amenities,
+    has_many :spot_amenities
 
     has_many :spots,
         through: :spot_amenities,
